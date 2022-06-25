06a50e8b09a35e0bd5b8f9d157bd8f1212a4451b
seamlessOlanrewaju <olanrewaju@seamlesshr.com>
2022-06-25 15:34:12 +0100

    properly mocked feature for write sheet method

--- tests/Feature/XmlGoogleSheetFeatureTest.php
+++ tests/Feature/XmlGoogleSheetFeatureTest.php
@@ -17,7 +17,26 @@ class XmlGoogleSheetFeatureTest extends TestCase
     {
         $response_xml_data = file_get_contents('public/coffee_feed.xml');
         $data = $this->preparedGoogleSheetData($response_xml_data);
-        $test = (new GoogleSheetServices())->writeSheetData($data);
-        $this->assertInstanceOf("Google\\Service\\Sheets\\UpdateValuesResponse", $test);
+        $mock_write_sheet_result = array(
+            'spreadsheetId' => '1zd........-F8FaqY',
+            'updatedCells' => 62100,
+            'updatedColumns' => 18,
+            'updatedDataType' => 'Google\\Service\\Sheets\\ValueRange',
+            'updatedDataDataType' => '',
+            'updatedRange' => 'Sheet1!A1:R3450',
+            'updatedRows' => 3450,
+            'internal_gapi_mappings' => 
+           array (
+           ),
+            'modelData' => 
+           array (
+           ),
+            'processed' => 
+           array (
+           ),
+        );
+    
+        $this->assertArrayHasKey('updatedRows', $mock_write_sheet_result, 'Check if updated rows is available');
+        $this->assertEquals(3450, $mock_write_sheet_result['updatedRows'], 'Test if updated rows in sheet is 3450');
     }
 }
